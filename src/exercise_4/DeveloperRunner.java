8a4a1b823e68a7c756f9bf4be9a412d0d8d3daaf
Kartik Chauhan <kartik@Kartiks-MacBook-Air.local>
2026-04-26 21:30:46 +0200

    Add DeveloperRunner to run Developer class for Exercise 4

---
+++ src/exercise_4/DeveloperRunner.java
@@ -0,0 +1,29 @@
+package exercise_4;
+
+public class DeveloperRunner {
+    public static void main(String[] args) {
+        System.out.println("--- Developer Demo ---");
+        
+        try {
+            // Creating a developer instance
+            Developer dev = new Developer("Kartik", 5, "Java", 50.0, true);
+            
+            // Testing methods
+            System.out.println("Info: " + dev.getInfo());
+            dev.code();
+            dev.debug();
+            dev.attendMeeting();
+            dev.drinkCoffee();
+            
+            System.out.println("\n--- Integrity Check Test ---");
+            try {
+                dev.setHourlyRate(-10);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Caught expected error: " + e.getMessage());
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
